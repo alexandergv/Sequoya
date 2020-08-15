4cf6ba2477e056802333fcc9e7504ecdfe55558c
k0o <sb.knot@gmail.com>
2020-08-14 22:29:53 -0400

    feat (*): responsive nav/side bar

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,44 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { PostService } from '../../../components/posts/post.service';
-import {MenuItem} from '../../../shared/models/menuItem.interface';
-
-
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
   public appName = 'Sequoya';
   public user: any;
+  public showSidebar: boolean;
+  public opened = false;
 
-  constructor(public authSvc: AuthService, public postSvc: PostService) { 
-   }
- 
-   
-
-    
+  constructor(public authSvc: AuthService, public postSvc: PostService) {}
   ngOnInit(): void {
-    this.authSvc.userData.subscribe( user =>{
-      if(user.displayName){
+    this.handleSidenav();
+    this.authSvc.userData.subscribe((user) => {
+      if (user.displayName) {
         this.user = user.displayName;
-      }
-      else{
+      } else {
         this.user = user.email;
       }
-      
-    })
-    
+    });
   }
 
-  onLogout(): void{
+  handleSidenav(){
+    this.showSidebar = (window.innerWidth < 610)
+  }
+
+  onLogout(): void {
     this.authSvc.logout();
-    
   }
-  onToggleSidenav(){
-    
+
+  OnClick() {
+    this.opened = !this.opened;
   }
+
 
 }
